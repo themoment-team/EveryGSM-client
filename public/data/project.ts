e50c3890c5afef50a80a83082e660d2b659bb21e
예빈 <yebin060310@gmail.com>
2023-04-04 23:20:07 +0900

    ADD data folder

---
+++ public/data/project.ts
@@ -0,0 +1,17 @@
+export const GCMS = [
+  {
+    projectName: 'GCMS',
+    projectDescription:
+      '광주소프트웨어마이스터고의 동아리를 관리해주는 서비스입니다',
+    imageURL: 'https://github.com/Junho-06.png',
+    createrDescription:
+      'https://www.notion.so/matsogeum/MSG-7ac3204e54e0484c9b1a63f472aa3e95?pvs=4',
+    projectUrl:
+      'https://gcms.msg-team.com/, https://apps.apple.com/kr/app/gcms/id1616315883, https://play.google.com/store/apps/details?id=com.msg.gcms',
+    githubRepoURL:
+      'https://github.com/GSM-MSG/MSG-BackEnd-V2, https://github.com/GSM-MSG/MSG-Front, https://github.com/GSM-MSG/GCMS-Android, https://github.com/GSM-MSG/GCMS-iOS ',
+    githubURL: 'https://github.com/Junho-06',
+    createrName: 'https://github.com/GSM-MSG',
+    categories: 'React, Spring, Kotlin, Swift',
+  },
+];
