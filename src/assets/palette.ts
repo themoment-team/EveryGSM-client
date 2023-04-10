6b991429a0aa9fb19317c755e539f4a1c1d05531
Lee-Seungje <moondgod@gmail.com>
2023-04-10 23:43:31 +0900

    Update palette

--- src/assets/palette.ts
+++ src/assets/palette.ts
@@ -1,5 +1,8 @@
-const P = {
-  gray: ['#F1F1F5', '#E3E3E3', '#999999', '#191919'],
+const theme = {
+  color: {
+    gray: ['#F1F1F5', '#E3E3E3', '#999999', '#191919'],
+  },
+  breakPoint: {},
 } as const;
 
-export default P;
+export default theme;
