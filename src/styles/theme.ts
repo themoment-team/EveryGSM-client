7f6788e98c4f8286cc26834130cad31d041ce534
Lee-Seungje <moondgod@gmail.com>
2023-04-21 08:14:46 +0900

    Update dark Card

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -17,12 +17,24 @@ export interface ThemeType {
     toggle: string;
     title: string;
     text: string;
+    card: string;
+    tag: string;
+    tag_text: string;
+    icon: string;
   };
   otherProp?: any;
 }
 
 export const theme: ThemeType = {
-  gray: ['#F1F1F5', '#E3E3E3', '#999999', '#636363', '#191919'],
+  gray: [
+    '#F1F1F5',
+    '#E3E3E3',
+    '#D9D9D9',
+    '#999999',
+    '#636363',
+    '#484848',
+    '#191919',
+  ],
   primary: {
     magenta: '#E23C96',
     pale_yellow: '#FFE870',
@@ -40,11 +52,23 @@ export const theme: ThemeType = {
     toggle: '#ffffff',
     title: '#000000',
     text: '#191919',
+    card: '#ffffff',
+    tag: '#f1f1f5',
+    tag_text: '#999999',
+    icon: '#D9D9D9',
   },
 } as const;
 
 export const darktheme: ThemeType = {
-  gray: ['#F1F1F5', '#E3E3E3', '#999999', '#636363', '#191919'],
+  gray: [
+    '#F1F1F5',
+    '#E3E3E3',
+    '#D9D9D9',
+    '#999999',
+    '#636363',
+    '#484848',
+    '#191919',
+  ],
   primary: {
     magenta: '#E23C96',
     pale_yellow: '#FFE870',
@@ -62,5 +86,9 @@ export const darktheme: ThemeType = {
     toggle: '#999999',
     title: '#ffffff',
     text: '#ffffff',
+    card: '#484848',
+    tag: '#636363',
+    tag_text: '#ffffff',
+    icon: '#e3e3e3',
   },
 } as const;
