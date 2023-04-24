ade1a51c81d3b226e123bf94fef1905198b401a5
Lee-Seungje <moondgod@gmail.com>
2023-04-24 09:01:24 +0900

    Add project Link

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,8 +4,9 @@ import * as I from 'assets/imgs';
 import * as C from 'components';
 import Image from 'next/image';
 import projectData from 'interface/projectData';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { css } from '@emotion/react';
+import Link from 'next/link';
 const Card = ({ data, index }: { data: projectData; index: number }) => {
   const [showModal, setShowModal] = useState<boolean>(false);
   const [selectedId, setSelectedId] = useState<string | null>(null);
@@ -52,61 +53,63 @@ const Card = ({ data, index }: { data: projectData; index: number }) => {
                 alt="Vector"
               />
             </S.DetailBtn>
-            <S.CardContents>
-              <S.Logo>
-                <Image
-                  src={data.imageURL}
-                  width={72}
-                  height={72}
-                  alt="로고이미지"
-                  css={css`
-                    border-radius: 100%;
-                  `}
-                ></Image>
-              </S.Logo>
-              <S.Title>{data.projectName}</S.Title>
-              <S.Create style={{ display: 'flex' }}>
-                <Image
-                  src={I.Person}
-                  css={css`
-                    width: 25px;
-                    height: 24px;
-                    margin-right: 13px;
-                  `}
-                  alt="Person"
-                />
-                <S.Creater>{data.createrName}</S.Creater>
-              </S.Create>
-              <S.Categories>
-                {data.categories.map(i => (
-                  <div key={i}>
-                    <C.Category data={i} />
-                  </div>
-                ))}
-              </S.Categories>
-            </S.CardContents>
+            <Link href={data.projectUrl}>
+              <S.CardContents>
+                <S.Logo>
+                  <Image
+                    src={data.imageURL}
+                    width={72}
+                    height={72}
+                    alt="로고이미지"
+                    css={css`
+                      border-radius: 100%;
+                    `}
+                  ></Image>
+                </S.Logo>
+                <S.Title>{data.projectName}</S.Title>
+                <S.Create style={{ display: 'flex' }}>
+                  <Image
+                    src={I.Person}
+                    css={css`
+                      width: 25px;
+                      height: 24px;
+                      margin-right: 13px;
+                    `}
+                    alt="Person"
+                  />
+                  <S.Creater>{data.createrName}</S.Creater>
+                </S.Create>
+                <S.Categories>
+                  {data.categories.map(i => (
+                    <div key={i}>
+                      <C.Category data={i} />
+                    </div>
+                  ))}
+                </S.Categories>
+              </S.CardContents>
+            </Link>
           </S.Desc>
         </S.Front>
         {/* <S.Back>
-                  <S.Title>{data.projectName}</S.Title>
-                  <S.Description>{data.createrDescription}</S.Description>
-                  <S.DetailBtn onClick={() => handleOpenModal(data.id)}>
-                  <div>상세보기</div>
-                  <Image
-                  src={I.Vector}
-                  style={{
-                    width: '6.2496px',
-                    height: '12.5008px',
-                    transform: 'rotateY(180deg)',
-                    marginLeft: '12px',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}
-                  alt="Vector"
-                  />
-                  </S.DetailBtn>
-                </S.Back> */}
+          <S.Title>{data.projectName}</S.Title>
+          <S.Description>{data.createrDescription}</S.Description>
+          <S.DetailBtn onClick={() => handleOpenModal(data.id)}>
+            <div>상세보기</div>
+            <Image
+              src={I.Vector}
+              style={{
+                width: '6.2496px',
+                height: '12.5008px',
+                transform: 'rotateY(180deg)',
+                marginLeft: '12px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              alt="Vector"
+            />
+          </S.DetailBtn>
+        </S.Back> */}
       </S.Card>
     </div>
   );
