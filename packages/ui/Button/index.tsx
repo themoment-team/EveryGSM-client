f9de8fcb937d207d24c52b0f2c876bd6637e8d4a
예빈 <yebin060310@gmail.com>
2023-06-07 09:54:00 +0900

    Update settings

--- packages/ui/Button/index.tsx
+++ packages/ui/Button/index.tsx
@@ -1,6 +1,33 @@
 import { css } from '@emotion/react';
+import { CustomButton } from './style';
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 
-const Button = () => {
+interface ButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  isLoading?: boolean;
+  width?: string;
+  bgColor?: string;
+  fontColor?: string;
+  hoverBgColor?: string;
+  position?: 'absolute' | 'relative' | 'fixed' | 'static' | 'sticky';
+  bottom?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children = '',
+  isLoading = false,
+  width,
+  bgColor,
+  fontColor,
+  hoverBgColor,
+  position,
+  bottom,
+  ...attributes
+}) => {
   return <div></div>;
 };
+
 export default Button;
