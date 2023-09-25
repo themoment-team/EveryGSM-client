b5a78ca8200560db4e4953c3f1bf7bc140ea9368
hyeongrok7874 <khr5k0228@gmail.com>
2023-09-25 16:21:13 +0900

    Update env

--- src/libs/gtag.ts
+++ src/libs/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID;
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
 // 페이지뷰 이벤트 전송
 export const pageview = (url: string) => {
