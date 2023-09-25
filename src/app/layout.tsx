ae0e3ffce4562c9b44b6781d157ce5737b07f676
Lee-Seungje <moondgod@gmail.com>
2023-09-25 13:50:11 +0900

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,31 @@
-'use client';
 import 'styles/global.css';
 
+import type { Metadata } from 'next';
+
 const GA_TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID;
+
+export const metadata: Metadata = {
+  applicationName: 'EveryGSM',
+  description: '광주소프트웨어마이스터고등학교 공식 홈페이지입니다.',
+  openGraph: {
+    title: '광주소프트웨어마이스터고등학교',
+    description: '광주소프트웨어마이스터고등학교 공식 홈페이지입니다.',
+    url: 'https://official.hellogsm.kr/',
+    siteName: 'official GSM',
+    images: [
+      {
+        url: '/public/images/Favicon.png',
+        width: 800,
+        height: 800,
+      },
+    ],
+    locale: 'ko',
+    type: 'website',
+  },
+  icons: {
+    icon: '/public/images/Favicon.png',
+  },
+};
 
 export default function RootLayout({
   children,
