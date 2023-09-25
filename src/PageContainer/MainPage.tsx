4daab1a1f1d18f62b8893c4e813cf98c37a1099b
Lee-Seungje <moondgod@gmail.com>
2023-09-25 14:54:04 +0900

    Delete use client in page.tsx

---
+++ src/PageContainer/MainPage.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useState } from 'react';
+
+import { ThemeProvider } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import { theme, darktheme } from 'styles/theme';
+import * as C from 'components';
+
+const AppWrapper = styled.div`
+  background-color: ${({ theme }) => theme.gray[0]};
+`;
+
+const MainPage = () => {
+  const [isDark, setIsDark] = useState<boolean>(false);
+
+  return (
+    <ThemeProvider theme={isDark ? darktheme : theme}>
+      <AppWrapper>
+        <C.Nav isDark={isDark} setIsDark={setIsDark} />
+        <C.MainContainer isDark={isDark} />
+        <C.Footer isDark={isDark} />
+      </AppWrapper>
+    </ThemeProvider>
+  );
+};
+
+export default MainPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
-'use client';
-
-import { useState } from 'react';
-
-import { ThemeProvider } from '@emotion/react';
-import styled from '@emotion/styled';
-
-import { theme, darktheme } from 'styles/theme';
-import * as C from 'components';
-
-const AppWrapper = styled.div`
-  background-color: ${({ theme }) => theme.gray[0]};
-`;
+import { MainPage } from 'PageContainer';
 
 function Home() {
-  const [isDark, setIsDark] = useState<boolean>(false);
-
-  return (
-    <ThemeProvider theme={isDark ? darktheme : theme}>
-      <AppWrapper>
-        <C.Nav isDark={isDark} setIsDark={setIsDark} />
-        <C.MainContainer isDark={isDark} />
-        <C.Footer isDark={isDark} />
-      </AppWrapper>
-    </ThemeProvider>
-  );
+  return <MainPage />;
 }
 
 export default Home;
