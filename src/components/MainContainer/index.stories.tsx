3debd69e0294d3470614c659300b424022b26802
Lee-Seungje <moondgod@gmail.com>
2023-09-26 15:18:08 +0900

    Add stories

---
+++ src/components/MainContainer/index.stories.tsx
@@ -0,0 +1,13 @@
+import MainContainer from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'MainContainer',
+  component: MainContainer,
+  parameters: {},
+} as Meta<typeof MainContainer>;
+
+type Story = StoryObj<typeof MainContainer>;
+
+export const Primary: Story = {};
