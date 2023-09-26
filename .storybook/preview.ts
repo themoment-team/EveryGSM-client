fa9acce2f9369e42ef0a71c8d6b7b5dbcb0be8c4
Lee-Seungje <moondgod@gmail.com>
2023-09-26 12:00:38 +0900

    Update storybook preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,6 +9,22 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: '#ffffff',
+        },
+        {
+          name: 'dark',
+          value: '#000000',
+        },
+      ],
+    },
+    nextjs: {
+      appDirectory: true,
+    },
   },
 };
 
