9d790a368dc60364bdb9f1e09b2c3c9c5ce6920b
Lee-Seungje <moondgod@gmail.com>
2023-10-10 12:21:49 +0900

    Add PCSlide

--- src/components/Slide/PC/index.tsx
+++ src/components/Slide/PC/index.tsx
@@ -4,11 +4,54 @@
 
 import { useState } from 'react';
 
+import Image from 'next/image';
+
+import project from 'constants/project.json';
+
+import * as S from './style';
+
 const PC = () => {
   const [slideIndex, setSlideIndex] = useState<number>(0);
-  const [tabletCardBox, setTabletCardBox] = useState<number>(3);
 
-  return <></>;
+  const handlePrevSlide = () => {
+    if (slideIndex === 0) {
+      setSlideIndex((project.length % 3) + 1);
+    } else {
+      setSlideIndex(slideIndex - 1);
+    }
+  };
+
+  const handleNextSlide = () => {
+    if (slideIndex > Math.floor(project.length / 3) - 1) {
+      setSlideIndex(0);
+    } else {
+      setSlideIndex(slideIndex + 1);
+    }
+  };
+
+  return (
+    <S.CardContainer>
+      <S.Container>
+        <Image
+          src="/images/Vector.svg"
+          alt="Vector"
+          onClick={handlePrevSlide}
+        />
+        <S.Cards>
+          {project.map((data, slideIndex) => (
+            <div key={slideIndex}>
+              <C.Card key={slideIndex + data.id} data={data} />
+            </div>
+          ))}
+        </S.Cards>
+        <Image
+          src="/images/Vector.svg"
+          alt="Vector"
+          onClick={handleNextSlide}
+        />
+      </S.Container>
+    </S.CardContainer>
+  );
 };
 
 export default PC;
--- src/components/Slide/PC/style.ts
+++ src/components/Slide/PC/style.ts
@@ -1,1 +1,31 @@
 import styled from '@emotion/styled';
+
+export const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5.75rem;
+  transition: ease-in-out 0.3s;
+  background-color: ${({ theme: { gray } }) => gray[0]};
+`;
+
+export const VectorWrapper = styled.div`
+  cursor: pointer;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 79.0625rem;
+`;
+
+export const Cards = styled.div`
+  width: 71.25rem;
+  height: 21.5625rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  overflow: hidden;
+`;
