3918345453decb7004a4bc97e0e34cb2af8ad584
Lee-Seungje <moondgod@gmail.com>
2023-10-18 10:51:27 +0900

    Update responsive

--- src/components/Slide/Tablet/style.ts
+++ src/components/Slide/Tablet/style.ts
@@ -6,6 +6,10 @@ export const CardContainer = styled.div`
   align-items: center;
   gap: 2.25rem;
   transition: ease-in-out 0.3s;
+
+  @media (max-width: 900px) {
+    gap: 0;
+  }
 `;
 
 export const VectorWrapper = styled.div<{ isRight?: boolean }>`
@@ -18,17 +22,16 @@ export const VectorWrapper = styled.div<{ isRight?: boolean }>`
 `;
 
 export const Cards = styled.div`
-  width: 90vw;
-  height: 100vh;
+  width: calc(76vw + 1.75rem);
+  height: calc(76vw + 1.75rem);
   position: relative;
-  /* overflow: hidden; */
+  overflow: hidden;
 `;
 
 export const MoveContainer = styled.div<{ slideIndex: number }>`
   display: grid;
-  grid-template-rows: repeat(2, 1fr); /* 2개의 행을 생성합니다. */
-  /* grid-template-columns: repeat(2, 1fr); /* 2개의 열을 생성합니다. */
-  /* grid-template-rows: repeat(2, 1fr);  */
+  grid-template-columns: repeat(10, 1fr);
+  grid-template-rows: repeat(2, 1fr);
   gap: 1.75rem;
   position: absolute;
   /* left: -${({ slideIndex }) => slideIndex * 72.75}rem; */
