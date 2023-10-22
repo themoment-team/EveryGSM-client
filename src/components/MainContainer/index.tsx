fec43f3cc9502b3c153215b0d55265f02278db31
Lee-Seungje <moondgod@gmail.com>
2023-10-23 02:16:07 +0900

    Delete useless code

--- src/components/MainContainer/index.tsx
+++ src/components/MainContainer/index.tsx
@@ -2,24 +2,13 @@
 
 'use client';
 
-import { useState, useEffect } from 'react';
-
-import { css } from '@emotion/react';
-
 import { Banner, TabletSlide, PCSlide, MobileSlide } from 'components';
-import project from 'constants/project.json';
 import { useWidthState } from 'stores';
 
 import * as S from './style';
 
-import type { DataType } from 'interface';
-
 const MainContainer = () => {
-  const [slideIndex, setSlideIndex] = useState<number>(0);
-  const [tabletCardBox, setTabletCardBox] = useState<number>(3);
   const { width } = useWidthState();
-
-  const getMaxIndex = () => Math.ceil(project.length / tabletCardBox) - 1;
 
   function getComponentByWidth(width: number) {
     if (width > 1150) {
@@ -30,54 +19,6 @@ const MainContainer = () => {
       return <MobileSlide />;
     }
   }
-
-  useEffect(() => {
-    if (width <= 1150) {
-      setTabletCardBox(4);
-    }
-  }, [width]);
-
-  const handlePrevSlide = () => {
-    if (slideIndex === 0) {
-      setSlideIndex(getMaxIndex());
-    } else {
-      setSlideIndex(curIndex => curIndex - 1);
-    }
-  };
-
-  const handleNextSlide = () => {
-    if (getMaxIndex() === slideIndex) {
-      setSlideIndex(0);
-    } else {
-      setSlideIndex(curIndex => curIndex + 1);
-    }
-  };
-
-  const tabletCardShow = (data = [{}], size = 1) => {
-    const cardDatas = [];
-
-    for (let i = 0; i < data.length; i += size) {
-      cardDatas.push(data.slice(i, i + size));
-    }
-
-    return cardDatas.map((array, i) => (
-      <div
-        key={i}
-        css={css`
-          display: flex;
-          width: 81vw;
-          height: 81vw;
-          flex-wrap: wrap;
-          justify-content: space-between;
-          align-content: space-between;
-        `}
-      >
-        {array.map((item, i) => (
-          <C.Card key={i} data={item as DataType} />
-        ))}
-      </div>
-    ));
-  };
 
   return (
     <S.MainBox>
