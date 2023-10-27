4d86186743e32aae1632e2cc1c21518331df409f
hyeongrok7874 <khr5k0228@gmail.com>
2023-10-27 11:46:03 +0900

    Update Vector svg

---
+++ src/assets/Vector.tsx
@@ -0,0 +1,19 @@
+const Vector = () => (
+  <svg
+    width="1rem"
+    height="2.125rem"
+    viewBox="0 0 16 34"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M14.25 32L1.75 17L14.25 2"
+      stroke="#999999"
+      stroke-width="3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+
+export default Vector;
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,4 @@ export { default as LogoIcon } from './LogoIcon';
 export { default as MoonIcon } from './MoonIcon';
 export { default as PersonIcon } from './PersonIcon';
 export { default as SunIcon } from './SunIcon';
+export { default as Vector } from './Vector';
