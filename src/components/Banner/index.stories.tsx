d0a3e2f2e0565b1db6ca7c57e5e1cfbf9b6872b9
Lee-Seungje <moondgod@gmail.com>
2023-10-30 19:23:21 +0900

    Delete useless code

--- src/components/Banner/index.stories.tsx
+++ src/components/Banner/index.stories.tsx
@@ -5,11 +5,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 export default {
   title: 'Banner',
   component: Banner,
-  parameters: {
-    backgrounds: {
-      default: 'dark',
-    },
-  },
 } as Meta<typeof Banner>;
 
 type Story = StoryObj<typeof Banner>;
