274ffe15a246b25264856ca60b2fd9b9cf7693ee
Lee-Seungje <moondgod@gmail.com>
2023-11-24 15:14:16 +0900

    Add filteredProjects

--- src/components/Slide/PC/index.tsx
+++ src/components/Slide/PC/index.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { Vector } from 'assets';
 import { Card } from 'components';
 import project from 'constants/project.json';
+import { useSearchState } from 'stores';
 
 import * as S from './style';
 
@@ -12,6 +13,8 @@ const CARDS_PER_PAGE = 3 as const;
 
 const PC = () => {
   const [slideIndex, setSlideIndex] = useState<number>(0);
+
+  const { searchKeyword } = useSearchState();
 
   const maxIndex = Math.ceil(project.length / CARDS_PER_PAGE) - 1;
 
@@ -31,6 +34,21 @@ const PC = () => {
     }
   };
 
+  const filteredProjects = project.filter(data => {
+    const values = Object.values(data).flatMap(value => {
+      if (typeof value === 'object' && value !== null) {
+        return Object.values(value);
+      }
+      return value;
+    });
+
+    return values.some(value => {
+      if (typeof value === 'string' && value.includes(searchKeyword)) {
+        return true;
+      }
+    });
+  });
+
   return (
     <S.CardContainer>
       <S.PrevController type="button" onClick={handlePrevSlide}>
@@ -38,7 +56,7 @@ const PC = () => {
       </S.PrevController>
       <S.Cards>
         <S.MoveContainer slideIndex={slideIndex}>
-          {project.map((data, slideIndex) => (
+          {filteredProjects.map((data, slideIndex) => (
             <Card key={slideIndex + data.id} data={data} />
           ))}
         </S.MoveContainer>
