4c701c4ce8e283715a135dbd1c06abd8a1a4730c
gaoooon <gaon9749@gmail.com>
2024-04-04 23:50:56 +0900

    Update left to translateX

--- src/components/Slide/Tablet/style.ts
+++ src/components/Slide/Tablet/style.ts
@@ -26,16 +26,15 @@ export const Cards = styled.div`
     76vw + 1.75rem
   ); // 한페이지의 카드 width * 카드 개수 + 카드 사이 gap
   height: calc(76vw + 1.75rem);
-  position: relative;
   overflow: hidden;
 `;
 
 export const Slider = styled.div<{ slideIndex: number }>`
-  position: absolute;
-  left: ${({ slideIndex }) =>
-    `calc(${slideIndex * -76}vw + ${
-      slideIndex * -3.5
-    }rem)`}; // 한페이지의 카드 width * 카드 개수 + 카드 사이 gap * 2
+  transform: translateX(
+    ${({ slideIndex }) =>
+      `calc(${slideIndex * -76}vw + ${slideIndex * -3.5}rem)`}
+  );
+  // 한페이지의 카드 width * 카드 개수 + 카드 사이 gap * 2
   transition: ease-in-out 0.3s;
 `;
 
